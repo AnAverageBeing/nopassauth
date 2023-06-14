97cadeba81ab8b2de6b6d127318f4bf1cb9c556a
AnAverageBeing <yatharthrathi.rathi@gamil.com>
2023-06-14 19:27:28 +0530

    wow fix and updates

--- auth/utils.go
+++ auth/utils.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
@@ -27,8 +28,8 @@ func EncryptWithPublicKey(data []byte, publicKey string) ([]byte, error) {
 		return nil, errors.New("failed to cast public key to RSA public key type")
 	}
 
-	// Encrypt the data using the public key
-	encryptedData, err := rsa.EncryptPKCS1v15(rand.Reader, rsaPublicKey, data)
+	// Encrypt the data using the public key with RSA OAEP and SHA256 hash
+	encryptedData, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, rsaPublicKey, data, nil)
 	if err != nil {
 		return nil, err
 	}
